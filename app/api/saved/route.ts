18f35d52a8116bd356bc9e7829f15c1e11fd286c
k8pai <thek8pai@gmail.com>
2023-10-16 04:32:20 +0530

    Updated Delete handler to support paths and tag validations

--- app/api/saved/route.ts
+++ app/api/saved/route.ts
@@ -1,24 +1,18 @@
 import prisma from '@/prisma';
-import { getServerSession } from 'next-auth';
-import { NextResponse } from 'next/server';
-import { authOptions } from '../auth/[...nextauth]/route';
+import { NextRequest, NextResponse } from 'next/server';
 import { Links } from '@prisma/client';
+import { revalidatePath, revalidateTag } from 'next/cache';
 
 export async function GET(request: Request) {
-	console.log('inside api route');
+	// console.log('inside api route');
 	try {
 		// console.log('email from delete api => ');
-		// const { email } = await request.json();
-		const session = await getServerSession(authOptions);
-		console.log('session => ', session);
-		if (!session?.user?.email) {
-			return NextResponse.json({ loginError: 'not logged in' });
-		}
-		const { email } = session.user;
+		const { email } = await request.json();
+		// const { email } = session.user;
 
 		const data: Links[] = await prisma.links.findMany({ where: { email } });
 		const response = data.reverse();
-		console.log('data => ', response);
+		// console.log('data => ', response);
 
 		return NextResponse.json(response);
 	} catch (error) {
@@ -26,14 +20,32 @@ export async function GET(request: Request) {
 	}
 }
 
-export async function DELETE(request: Request) {
-	console.log('inside delete api');
+export async function POST(request: Request) {
+	try {
+		// console.log('email from delete api => ');
+		const { email } = await request.json();
+		// const { email } = session.user;
+
+		const data: Links[] = await prisma.links.findMany({ where: { email } });
+		const response = data.reverse();
+		// console.log('data => ', response);
+
+		revalidatePath('/saved');
+
+		return NextResponse.json(response);
+	} catch (error) {
+		return NextResponse.json(error);
+	}
+}
+
+export async function DELETE(request: NextRequest) {
 	try {
 		const { id } = await request.json();
-		console.log('id from delete api => ', id);
+		// console.log('id from delete api => ', id);
 		const deletedItem: Links = await prisma.links.delete({
 			where: { id: id },
 		});
+		// console.log('deletedItem => ', deletedItem);
 		return NextResponse.json(deletedItem);
 	} catch (error) {
 		return NextResponse.json(error);
